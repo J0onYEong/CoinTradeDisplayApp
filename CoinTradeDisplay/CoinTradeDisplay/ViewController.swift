caef7907a144d65453ba84b061052c71a2157f74
J0onYEong <cjy2134@naver.com>
2024-08-23 09:24:46 +0900

    Initial commit

---
+++ CoinTradeDisplay/CoinTradeDisplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinTradeDisplay
+//
+//  Created by choijunios on 8/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
