20f3d3c3eabc962a1e3da75848e550096c40d5e6
J0onYEong <cjy2134@naver.com>
2024-08-30 13:08:22 +0900

    [Fix] 주석 수정

--- CoinTradeDisplay/Domain/RepositoryInterface/RepositoryInterface.swift
+++ CoinTradeDisplay/Domain/RepositoryInterface/RepositoryInterface.swift
@@ -10,10 +10,13 @@ import RxSwift
 
 public protocol OrderBook2Repository {
     
-    /// 데이터를 지속적으로 가져옵니다.
-    /// - parameters
-    ///     bufferSize: 한번에가져오는 데이터의 양을 의미합니다.
-    ///     timeSpan: 데이터를 수신받는 주기를 의미합니다. (밀리세컨)
+    /**     
+    스트림의 데이터 흐름을 설정합니다.
+     > 스트림의 데이터 흐름을 설정합니다.
+     - Parameters:
+        - bufferSize: 한 번에 가져오는 데이터의 양을 의미합니다.
+        - timeSpan: 데이터를 수신받는 주기를 의미합니다. (밀리초 단위)
+    */
     func setSream(bufferSize: Int, timeSpan: Int)
     
     /// 웹소켓 스트림을 시작합니다.
